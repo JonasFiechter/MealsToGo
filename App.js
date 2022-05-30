3dc681f3b84fc26ccd45e21ad2e0189e051d9963
JonasFiechter <jonasrfiechter@gmail.com>
2022-05-29 22:18:06 -0300

    Now its working fine

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <View>
+      <View style={styles.header}>
         <Text>HEADER</Text>
       </View>
-      <View>
+      <View style={styles.body}>
         <Text>BODY</Text>
       </View>
-      <View>
+      <View style={styles.menu}>
         <Text>MENU</Text>
       </View>
     </SafeAreaView>
@@ -21,5 +20,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#222',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight  : 0,
+  },
+  header: {
+    flex: 1,
+    flexGrow: 0.1,
+    backgroundColor: '#948',
+  },
+  body: {
+    flex: 1,
+    flexGrow: 1,
+    backgroundColor: '#448',
+  },
+  menu: {
+    flex: 1,
+    flexGrow: 0.1,
+    backgroundColor: '#148',
   },
 });
