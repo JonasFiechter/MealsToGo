e9e9ba6c61b532b9aef98de12302042cae46652d
JonasFiechter <jonasrfiechter@gmail.com>
2022-06-01 11:01:58 -0300

    .

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { RestaurantsScreen } from './src/features/restaurant/screens/restaurant_screen.js';
 import { ThemeProvider } from 'styled-components/native';
 import { theme } from './src/infraestructure/theme/index.js';
-import { useFonts as useOswald, Oswald_300Light } from '@expo-google-fonts/oswald';
+import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald';
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
-    Oswald_300Light,
+    Oswald_400Regular,
   });
 
   const [latoLoaded] = useLato({
