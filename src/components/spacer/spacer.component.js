848488c1e8724326c682964aac07ebe313618a5f
JonasFiechter <jonasrfiechter@gmail.com>
2022-06-04 11:49:52 -0300

    spacer component

---
+++ src/components/spacer/spacer.component.js
@@ -0,0 +1,34 @@
+import react from "react";
+import { View } from 'react-native';
+import styled from 'styled-components/native'
+
+const TopSmall = styled.View`
+    marginTop: 4px;
+`;
+
+const TopMedium = styled.View`
+    marginTop: 8px;
+`;
+
+const TopLarge = styled.View`
+    marginTop: 16px;
+`;
+
+const LeftSmall = styled.View`
+    marginLeft: 4px;
+`;
+
+const LeftMedium = styled.View`
+    marginLeft: 8px;
+`;
+
+const LeftLarge = styled.View`
+    marginLeft: 16px;
+`;
+
+
+export const Spacer = ({variant}) => {
+    if(variant === 'top.small') {
+        return <TopSmall/>
+    }
+};
